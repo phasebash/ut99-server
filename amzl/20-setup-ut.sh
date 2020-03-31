7ea0f88955c5dd5daffe08d95b8b83cfd68c406e
Brandon Zeeb <zeeb.brandon@gmail.com>
2020-03-30 22:15:23 -0400

    bring amzl inline with docker.

--- amzl/20-setup-ut.sh
+++ amzl/20-setup-ut.sh
@@ -1,43 +1,59 @@
-#!/usr/bin/env bash
-set -eux
-
-pwd
-find .
+#!/usr/bin/env sh
+set -eu
 
 echo "setting up UT99"
-aws s3 cp s3://${S3_ARIFACTS_BUCKET}/ut-server-436.tar.gz  .
+aws s3 cp "s3://${S3_ARTIFACTS_BUCKET}/ut-server-436.tar.gz"  .
 
 tar -zxf ut-server-436.tar.gz
-pushd ut-server
+cd ut-server
 chmod +x ucc
 
 echo "patching UT99"
-aws s3 cp s3://${S3_ARIFACTS_BUCKET}/UTPGPatch451LINUX.tar.tar  .
+aws s3 cp "s3://${S3_ARTIFACTS_BUCKET}/UTPGPatch451LINUX.tar.tar"  .
 tar xfj UTPGPatch451LINUX.tar.tar
 rm UTPGPatch451LINUX.tar.tar
 
 echo "installing asu addon"
-aws s3 cp s3://${S3_ARIFACTS_BUCKET}/asu-0.6.tar.gz .
+aws s3 cp "s3://${S3_ARTIFACTS_BUCKET}/asu-0.6.tar.gz" .
 tar -zxf asu-0.6.tar.gz
 rm asu-0.6.tar.gz
 chmod +x asu.sh
 
 echo "
 I
-
+ec2-user
 
 1
 
 
 
+P
+y
+n
+n
+n
+y
+O
+C
+20000
+I
+120
 X
-" | ./asu.sh
+X
+" | bash ./asu.sh
 
 echo "fixing System"
-pushd System
-ln -s libSDL-1.1.so.0 libSDL-1.2.so.0
+cd System
 chmod +x ucc-bin
-popd
 
-echo "unpacking UTPUre"
-tar xzvf Patches/UTPure-RC5v-patch.tar.gz
+echo "Reconfiguring UnrealTournament.ini"
+sed -i "s/ServerName=.*/ServerName=${SERVER_NAME}/g" UnrealTournament.ini
+sed -i "s/ShortName=.*/ShortName=${SERVER_SHORT_NAME}/g" UnrealTournament.ini
+sed -i "s/AdminUsername=.*/AdminUsername=${ADMIN_USERNAME}/g" UnrealTournament.ini
+sed -i "s/AdminPassword=.*/AdminPassword=${ADMIN_PASSWORD}/g" UnrealTournament.ini
+sed -i "s/bEnabled=.*/bEnabled=True\nListenPort=8080\nServerName=${SERVER_NAME}/g" UnrealTournament.ini
+
+cd ..
+
+echo "chowning for ec2-user"
+chown -R ec2-user:ec2-user .
